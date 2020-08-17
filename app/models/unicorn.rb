3228d34a564bca41463f959fe4813530747aced7
lea mousel <leamousel51@gmail.com>
2020-08-17 15:06:04 +0200

    Add dotenv - Protect my secret data in .env file

--- app/models/unicorn.rb
+++ app/models/unicorn.rb
@@ -1,3 +1,16 @@
 class Unicorn < ApplicationRecord
   has_many :bookings
+  belongs_to :user
+
+
+
+  MAGIC_POWERS = ["flying", "healing", "garden blooming", "yogi master", "fortune-telling"]
+
+  validates :name, presence: true
+  validates :magic_power, presence: true, inclusion: { in: MAGIC_POWERS,
+      message: "%{value} is not a valid magic_power" }
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :location, presence: true
+
 end
