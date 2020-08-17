e00baaf4634a2beb85285fe2e639976cb3c5612b
lea mousel <leamousel51@gmail.com>
2020-08-17 13:04:27 +0200

    add booking model - waiting for adding user model to add user foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_105152) do
+ActiveRecord::Schema.define(version: 2020_08_17_110319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "rating"
+    t.text "review"
+    t.bigint "unicorn_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["unicorn_id"], name: "index_bookings_on_unicorn_id"
+  end
 
   create_table "unicorns", force: :cascade do |t|
     t.string "name"
@@ -25,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_08_17_105152) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "bookings", "unicorns"
 end
