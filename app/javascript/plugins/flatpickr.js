8f6f8381fe8d652df467803ecf9543112a106ae7
lea mousel <leamousel51@gmail.com>
2020-08-18 16:50:10 +0200

    calendar ok - get booking duration ongoing

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,26 @@
 import flatpickr from "flatpickr";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
-flatpickr("#range_start", {
-  plugins: [new rangePlugin({ input: "#range_end"})]
+const startDate = document.querySelector("#booking-start");
+const endDate = document.querySelector("#booking-end");
+const bookingDuration = document.querySelector("#booking-duration");
+
+
+flatpickr("#start_date", {
+  onChange: function(selectedDates, dateStr, instance) {
+    end_date.set('minDate',dateStr)
+  },
+  onClose: function(selectedDates, dateStr, instance) {
+    startDate.value = dateStr
+  },
+  dateFormat: "Y-m-d",
+  minDate: "today"
+})
+
+
+const end_date = flatpickr("#end_date", {
+  onClose: function(selectedDates, dateStr, instance) {
+    endDate.value = dateStr
+  },
+  dateFormat: "Y-m-d",
 });
