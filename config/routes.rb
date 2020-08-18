e05a211c8bd507dfca2256dad6280a93e81f02fb
Ines Delbecq <ines.delbecq@gmail.com>
2020-08-18 11:26:37 +0200

    added alias to show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/unicorns', to: "unicorns#index"
-  get '/unicorns/:id', to: 'unicorns#show'
+  get '/unicorns/:id', to: 'unicorns#show', as: :unicorn_show
   get '/unicorns/:id', to: "booking#new", as: :new_booking
   get '/unicorns/new', to: 'unicorns#new', as: :new_unicorn
   post '/unicorns', to: 'unicorns#create'
