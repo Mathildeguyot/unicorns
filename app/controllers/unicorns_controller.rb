0d0a93a2498dc12ef746c35406c34632ed1ca780
Ines Delbecq <ines.delbecq@gmail.com>
2020-08-18 10:34:23 +0200

    added controller actions

--- app/controllers/unicorns_controller.rb
+++ app/controllers/unicorns_controller.rb
@@ -1,2 +1,23 @@
 class UnicornsController < ApplicationController
+
+  def new
+    @unicorn = Unicorn.new
+  end
+
+  def create
+    @unicorn = Unicorn.new(params_unicorn)
+    if @unicorn.save
+      redirect_to unicorn_path(@unicorn)
+    else
+      render :new
+    end
+  end
+
+  def params_unicorn
+    params.require(:unicorn).permit(:name, :magic_power, :description, :price, :location, :photos)
+  end
+
+  def set_unicorn
+    @unicorn = Unicorn.find(params[:id])
+  end
 end
