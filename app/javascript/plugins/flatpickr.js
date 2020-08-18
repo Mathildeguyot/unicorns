44dbcbc1b79af3ed4bd871ec9836852afcdaac51
lea mousel <leamousel51@gmail.com>
2020-08-18 17:50:19 +0200

    calculating booking price depending on selected dates

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,14 +4,32 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 const startDate = document.querySelector("#booking-start");
 const endDate = document.querySelector("#booking-end");
 const bookingDuration = document.querySelector("#booking-duration");
+const unicornPrice= document.querySelector("#unicorn-price");
+const unicornTotalPrice= document.querySelector("#unicorn-total-price");
+const serviceFees= document.querySelector("#service-fees");
+const bookingTotalPrice= document.querySelector("#booking-total-price");
 
+const days_between=(date1, date2) => {
+  // The number of milliseconds in one day
+    const ONE_DAY = 1000 * 60 * 60 * 24
+
+    // Convert both dates to milliseconds
+    const date1_ms = date1.getTime();
+    const date2_ms = date2.getTime();
+
+    // Calculate the difference in milliseconds
+    const difference_ms = Math.abs(date1_ms - date2_ms)
+
+    // Convert back to days and return
+    return Math.round(difference_ms/ONE_DAY)
+};
 
 flatpickr("#start_date", {
   onChange: function(selectedDates, dateStr, instance) {
     end_date.set('minDate',dateStr)
   },
   onClose: function(selectedDates, dateStr, instance) {
-    startDate.value = dateStr
+    startDate.value = dateStr;
   },
   dateFormat: "Y-m-d",
   minDate: "today"
@@ -20,7 +38,14 @@ flatpickr("#start_date", {
 
 const end_date = flatpickr("#end_date", {
   onClose: function(selectedDates, dateStr, instance) {
-    endDate.value = dateStr
+    endDate.value = dateStr;
+    const start = new Date(startDate.value);
+    const end = new Date(endDate.value);
+    bookingDuration.innerText = ` x ${days_between(start, end)}j`;
+    unicornTotalPrice.innerText = `${parseFloat(unicornPrice.innerText) * days_between(start, end)}€`;
+    serviceFees.innerText = `${(parseFloat(unicornTotalPrice.innerText) * 0.05).toFixed(2) }€`;
+    bookingTotalPrice.innerText = `${(parseFloat(unicornTotalPrice.innerText) +parseFloat(serviceFees.innerText)).toFixed(2)}€`;
+
   },
   dateFormat: "Y-m-d",
 });
