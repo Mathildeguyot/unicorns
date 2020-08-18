f8290ec06a200c37b010160b75590ddd07729a1d
Mathilde Guyot <mathilde.guyot1@gmail.com>
2020-08-18 11:18:28 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/unicorns_controller.rb
+++ app/controllers/unicorns_controller.rb
@@ -1,5 +1,10 @@
 class UnicornsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :show
   def show
-    #
+    @unicorn = Unicorn.find(params[:id])
+  end
+
+  def unicorn_params
+    params.require(:unicorn).permit(:name, :magic_power, :rating, :description, :location, :price, :user)
   end
 end
