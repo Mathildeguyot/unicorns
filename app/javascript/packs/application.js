171d0a0712c01e8994a7893204f3bfc0a44e7145
Mathilde Guyot <mathilde.guyot1@gmail.com>
2020-08-21 12:01:47 +0200

    seed okay

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,6 +32,7 @@ import { bookUnicorn } from '../components/fetch_current_location';
 
 import { initFixBookingCardOnScroll } from '../components/booking-card';
 import markerhover from '../components/hovering_markers.js'
+import cardhover from '../components/hovering_cards.js'
 import { initAutocomplete } from '../plugins/init_autocomplete';
 
 // Fixing booking card when scrolling
@@ -56,20 +57,7 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   if (document.querySelector("#map")) {
     markerhover();
-    const markersgroup = document.querySelectorAll(".marker");
-    const cardsgroup = document.querySelectorAll(".unicorn-index-card");
-    cardsgroup.forEach((card) => {
-      card.addEventListener("mouseover", (event) => {
-        card.classList.add("special-class")
-        document.querySelector(`.marker .${card.id}`).width = "50px";
-        document.querySelector(`.marker .${card.id}`).height = "50px";
-      });
-      card.addEventListener("mouseout", (event) => {
-        card.classList.remove("special-class")
-        document.querySelector(`.marker .${card.id}`).width = "25px";
-        document.querySelector(`.marker .${card.id}`).height = "25px";
-      });
-    });
+    cardhover();
   };
 });
 
